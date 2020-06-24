14ccec3e6c74a9cac8cf3664b6094164ac7f0991
Yurii Nosachenko <yurik_n@outlook.com>
2020-06-24 10:05:50 +0300

    Create EmojiModel.swift

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by yurik on 6/23/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import Foundation
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isChekked: Bool
+}
